2c06181a12a5ba75b816ed3d9bac1aaef3e7111d
jchene <jchene@student.42.fr>
2021-11-10 00:32:04 +0100

    new_gnl

--- exam02/new_gnl/get_next_line.h
+++ exam02/new_gnl/get_next_line.h
@@ -6,13 +6,12 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 19:26:31 by jchene            #+#    #+#             */
-/*   Updated: 2021/11/09 19:36:44 by jchene           ###   ########.fr       */
+/*   Updated: 2021/11/10 00:32:01 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 5
 # include <unistd.h>
 # include <stdlib.h>
 
